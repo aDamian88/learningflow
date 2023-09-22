34c024b707cc14dfe8af0b38ff07fdea7051167a
a.damianakis <alexandros.damianakis@external.toyotaconnected.eu>
2023-09-22 14:23:18 +0300

    Checking the state.

--- app/src/main/java/com/adamian/learningflow/MainActivity.kt
+++ app/src/main/java/com/adamian/learningflow/MainActivity.kt
@@ -3,6 +3,8 @@ package com.adamian.learningflow
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,6 +19,8 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
@@ -33,6 +37,7 @@ import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.adamian.learningflow.ui.theme.BottomNavItem
 import com.adamian.learningflow.ui.theme.LearningflowTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
@@ -166,12 +171,30 @@ fun HomeScreen() {
 
 @Composable
 fun ChatScreen() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(text = "Chat screen")
+    Column(Modifier.fillMaxSize()) {
+
+        val color = remember {
+            mutableStateOf(Color.Yellow)
+        }
+        Box(
+            modifier = Modifier
+                .background(color.value)
+                .weight(1f)
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "Chat screen")
+        }
+        ColorBox(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxSize()
+        ) {
+            color.value = it
+        }
     }
+
+
 }
 
 @Composable
@@ -182,4 +205,28 @@ fun SettingsScreen() {
     ) {
         Text(text = "Settings screen")
     }
+}
+
+@Composable
+fun ColorBox(
+    modifier: Modifier = Modifier,
+    updateColor: (Color) -> Unit
+) {
+//    val color = remember {
+//        mutableStateOf(Color.Yellow)
+//    }
+
+    Box(modifier = modifier
+        .background(Color.Red)
+        .clickable {
+            updateColor(
+                Color(
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    1f
+                )
+            )
+        })
+
 }
